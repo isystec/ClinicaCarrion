9a430fa628d691a79cde5fce1c05d9799a5098c6
RedJohn1426 <alex98rodriguezromero@gmail.com>
2019-05-10 12:23:51 -0500

    Se corrige funcion calculaedad

--- resources/views/hclinica/create.blade.php
+++ resources/views/hclinica/create.blade.php
@@ -1,13 +1,10 @@
 <?php
-  function calculaedad($fechanacimiento){
-    list($ano,$mes,$dia) = explode("-",$fechanacimiento);
-    $ano_diferencia = date("Y") - $ano;
-    $mes_diferencia = date("m") - $mes;
-    $dia_diferencia = date("d") - $dia;
-    if ($dia_diferencia < 0 && $mes_diferencia <= 0)
-    $ano_diferencia--;
-    return $ano_diferencia;
-  }
+      function calculaedad($fechanacimiento){
+        $fecha_nacimiento = new DateTime($fechanacimiento);
+        $hoy = new DateTime();
+        $annos = $hoy->diff($fecha_nacimiento);
+        return $annos->y;
+      }
 ?>
 @extends('plantilla.plantilla')
 @section('contenido')
--- resources/views/hclinica/pdf.blade.php
+++ resources/views/hclinica/pdf.blade.php
@@ -1,12 +1,9 @@
 <?php
   function calculaedad($fechanacimiento){
-    list($ano,$mes,$dia) = explode("-",$fechanacimiento);
-    $ano_diferencia = date("Y") - $ano;
-    $mes_diferencia = date("m") - $mes;
-    $dia_diferencia = date("d") - $dia;
-    if ($dia_diferencia < 0 && $mes_diferencia <= 0)
-    $ano_diferencia--;
-    return $ano_diferencia;
+    $fecha_nacimiento = new DateTime($fechanacimiento);
+    $hoy = new DateTime();
+    $annos = $hoy->diff($fecha_nacimiento);
+    return $annos->y;
   }
 ?>
 <!DOCTYPE html>
